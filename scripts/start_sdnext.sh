f7d437933fd47969e0a8bccbdf1c036088f09617
mnb3000 <misha.mykolyshyn@gmail.com>
2024-12-31 10:56:27 +0200

    feat(backend): use xformers, add mising env vars

--- scripts/start_sdnext.sh
+++ scripts/start_sdnext.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "SDNext: Starting automatic"
 cd /workspace/"${APP}" || exit
-python launch.py --debug --listen --insecure --api-log --log sdnext.log
+python launch.py --debug --listen --insecure --api-log --log sdnext.log --use-xformers
