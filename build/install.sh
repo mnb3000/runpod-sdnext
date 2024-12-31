a7ec0a7a17e53e4f6003425cc7a49700d4f4a541
mnb3000 <misha.mykolyshyn@gmail.com>
2024-12-31 14:35:03 +0200

    build(extensions): temporary hack to store extensions as built-in

--- build/install.sh
+++ build/install.sh
@@ -18,17 +18,17 @@ pip install -r requirements.txt
 python /automatic/launch.py --debug --use-cuda --use-xformers --test --optional --insecure
 
 # Clone the extensions
-git clone https://github.com/Mikubill/sd-webui-controlnet.git extensions/sd-webui-controlnet
-git clone https://github.com/continue-revolution/sd-webui-animatediff extensions/sd-webui-animatediff
-git clone --depth=1 https://github.com/civitai/sd_civitai_extension.git extensions/sd_civitai_extension
-git clone https://github.com/BlafKing/sd-civitai-browser-plus.git extensions/sd-civitai-browser-plus
+git clone https://github.com/Mikubill/sd-webui-controlnet.git extensions_builtin/sd-webui-controlnet
+git clone https://github.com/continue-revolution/sd-webui-animatediff extensions_builtin/sd-webui-animatediff
+git clone --depth=1 https://github.com/civitai/sd_civitai_extension.git extensions_builtin/sd_civitai_extension
+git clone https://github.com/BlafKing/sd-civitai-browser-plus.git extensions_builtin/sd-civitai-browser-plus
 
 # Install dependencies for the various extensions
-cd "/automatic/extensions/sd-webui-controlnet"
+cd "/automatic/extensions_builtin/sd-webui-controlnet"
 pip install -r requirements.txt
-cd "/automatic/extensions/sd_civitai_extension"
+cd "/automatic/extensions_builtin/sd_civitai_extension"
 pip install -r requirements.txt
 
 # Install dependencies for Civitai Browser+ extension
-cd "/automatic/extensions/sd-civitai-browser-plus"
+cd "/automatic/extensions_builtin/sd-civitai-browser-plus"
 pip install send2trash beautifulsoup4 ZipUnicode fake-useragent packaging pysocks
