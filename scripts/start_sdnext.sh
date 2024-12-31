8ae8aae7bf8335a584015d58052daeb92da7450d
mnb3000 <misha.mykolyshyn@gmail.com>
2024-12-31 12:35:04 +0200

    ci(testing): update `$RELEASE` on test builds

--- scripts/start_sdnext.sh
+++ scripts/start_sdnext.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "SDNext: Starting automatic"
 cd /workspace/"${APP}" || exit
-python launch.py --debug --listen --skip-all --insecure --api-log --log sdnext.log --use-xformers
+python launch.py --debug --listen --skip-all --insecure --use-xformers --api-log --log sdnext.log
