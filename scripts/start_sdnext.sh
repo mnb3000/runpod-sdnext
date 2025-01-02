c69d1a0b0ba8f8f53dcd7aeccbcd921f9291fd04
mnb3000 <misha.mykolyshyn@gmail.com>
2025-01-02 02:53:44 +0200

    feat(backend): switch to diffusers backend

--- scripts/start_sdnext.sh
+++ scripts/start_sdnext.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "SDNext: Starting automatic"
 cd /workspace/"${SD_BASEDIR}" || exit
-venv_dir=$VENV_PATH ./webui.sh --debug --listen --skip-all --insecure --use-cuda --use-xformers --backend=original --api-log --log sdnext.log
+venv_dir=$VENV_PATH ./webui.sh --debug --listen --skip-all --insecure --use-cuda --use-xformers --api-log --log sdnext.log
