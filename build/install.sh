c8ec9fd38fd51ae4f9e46c3f0a9495d3c9f3b478
mnb3000 <misha.mykolyshyn@gmail.com>
2025-01-02 05:45:40 +0200

    fix(install): fix extensions paths

--- build/install.sh
+++ build/install.sh
@@ -30,11 +30,11 @@ pip install -r requirements.txt
 python /automatic/launch.py --debug --use-cuda --use-xformers --test --optional --insecure
 
 # Install dependencies for the various extensions
-cd "/automatic/extensions_builtin/sd-webui-controlnet"
+cd "/automatic/extensions-builtin/sd-webui-controlnet"
 pip install -r requirements.txt
-cd "/automatic/extensions_builtin/sd_civitai_extension"
+cd "/automatic/extensions-builtin/sd_civitai_extension"
 pip install -r requirements.txt
 
 # Install dependencies for Civitai Browser+ extension
-cd "/automatic/extensions_builtin/sd-civitai-browser-plus"
+cd "/automatic/extensions-builtin/sd-civitai-browser-plus"
 pip install send2trash beautifulsoup4 ZipUnicode fake-useragent packaging pysocks
